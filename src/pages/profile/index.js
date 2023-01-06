eeb49e165a0e63bb8c5bef9a3bdc0f2567b97ce7
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-07 00:45:46 +0700

    Add profile page

---
+++ src/pages/profile/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Navbar from "../../components/module/navbar";
+import { getCart } from "../../redux/actions/cart.action";
+
+import styles from "./profile.module.css";
+
+const Profile = () => {
+  const dispatch = useDispatch();
+
+  const { cart } = useSelector((state) => state.cart);
+
+  const uid = localStorage.getItem("uid");
+
+  const fetchAPI = () => {
+    dispatch(getCart(uid));
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <Fragment>
+      <Navbar />
+      <main className={styles.main}>
+        <h1>Profile Page</h1>
+      </main>
+      <div>
+        {cart?.map((item) => (item.status !== 0 ? <li>{item.title}</li> : ""))}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Profile;
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "../pages/auth/register";
 import Login from "../pages/auth/login";
-import Cart from "../pages/cart";
-import Detail from "../pages/product/detail";
 import List from "../pages/product/list";
+import Detail from "../pages/product/detail";
+import Cart from "../pages/cart";
+import Profile from "../pages/profile";
 
 const Router = () => {
   return (
@@ -16,6 +17,7 @@ const Router = () => {
         <Route path="/product" element={<List />} />
         <Route path="/product/:id" element={<Detail />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
