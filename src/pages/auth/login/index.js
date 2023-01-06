334bb0882e32a2da1520448608c03deb6208ef27
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-06 12:04:31 +0700

    Add email and google login to login page

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,10 +1,90 @@
-import React from 'react'
-import styles from "../auth.module.css"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import app from "../../../firebase";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
 
-export default Login
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleEmailLogin = () => {
+    signInWithEmailAndPassword(auth, form.email, form.password)
+      .then((res) => {
+        const user = {
+          uid: res.user.uid,
+          email: res.user.email,
+          name: res.user.displayName,
+        };
+        const token = res.user.accessToken;
+        localStorage.setItem("token", token);
+        alert(`Welcome, ${user.name}`);
+        navigate("/product");
+      })
+      .catch((err) => {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+        console.log(errorCode, errorMessage);
+        alert(errorMessage);
+      });
+  };
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        const credential = GoogleAuthProvider.credentialFromResult(res);
+        const token = credential.accessToken;
+        console.log(token);
+        const user = {
+          id: res.user.uid,
+          email: res.user.email,
+          name: res.user.displayName,
+        };
+
+        localStorage.setItem("token", token);
+        alert(`Welcome, ${user.name}`);
+        navigate("/product");
+      })
+      .catch((err) => {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+        const credential = GoogleAuthProvider.credentialFromError(err);
+        console.log(errorCode, errorMessage, credential);
+        alert(errorMessage);
+      });
+  };
+
+  return (
+    <main>
+      <h1>Login Page</h1>
+
+      <button onClick={() => handleGoogleLogin()}>Google</button>
+      <hr />
+      <input
+        type="email"
+        placeholder="email"
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+        value={form.email}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+        value={form.password}
+      />
+      <button onClick={() => handleEmailLogin()}>Log In</button>
+    </main>
+  );
+};
+
+export default Login;
