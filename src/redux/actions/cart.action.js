613650bc807ba4f25a712400edd8e063f773f94c
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-07 00:22:27 +0700

    Add get, insert and remove cart function

---
+++ src/redux/actions/cart.action.js
@@ -0,0 +1,121 @@
+import axios from "axios";
+import swal from "sweetalert";
+import {
+  GET_CART_PENDING,
+  GET_CART_SUCCESS,
+  GET_CART_ERROR,
+  INSERT_CART_PENDING,
+  INSERT_CART_SUCCESS,
+  INSERT_CART_ERROR,
+  REMOVE_CART_ERROR,
+  REMOVE_CART_PENDING,
+  REMOVE_CART_SUCCESS,
+} from "../types";
+
+export const getCart = (uid) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_CART_PENDING });
+
+    const result = await axios.get(
+      `${process.env.REACT_APP_API_URL}/cart/${uid}`
+    );
+
+    console.log(result);
+
+    const { msg } = result.data;
+    const { cart } = result.data;
+
+    console.log(msg);
+    dispatch({ type: GET_CART_SUCCESS, payload: cart });
+  } catch (err) {
+    const errorCode = err.code;
+    const errorMessage = err.message;
+    console.log(errorCode, errorMessage);
+
+    swal({
+      title: "Request Error",
+      text: `Please try again in a few minutes`,
+      icon: "error",
+    });
+
+    dispatch({ type: GET_CART_ERROR });
+  }
+};
+
+export const insertCart = (form, token) => async (dispatch) => {
+  try {
+    dispatch({ type: INSERT_CART_PENDING });
+    await axios.post(`${process.env.REACT_APP_API_URL}/cart`, form, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    swal({
+      title: "Added to cart",
+      text: `Product successfully added to cart`,
+      timer: 1000,
+      icon: "success",
+    });
+    dispatch({ type: INSERT_CART_SUCCESS });
+  } catch (err) {
+    const errorCode = err.code;
+    const errorMessage = err.message;
+    console.log(errorCode, errorMessage);
+
+    swal({
+      title: "Request Error",
+      text: `Please try again in a few minutes`,
+      icon: "error",
+    });
+
+    dispatch({ type: INSERT_CART_ERROR });
+  }
+};
+
+export const removeCart = (id, uid, token) => async (dispatch) => {
+  try {
+    dispatch({ type: REMOVE_CART_PENDING });
+
+    await swal({
+      title: "Remove Item",
+      text: `Are you sure want to remove this item?`,
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then(async (confirm) => {
+      if (confirm) {
+        await axios.delete(`${process.env.REACT_APP_API_URL}/cart/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        swal({
+          title: "Item removed",
+          text: `Item successfully removed from your card`,
+          icon: "success",
+          timer: 1800,
+          buttons: false,
+        });
+      }
+    });
+
+    const result = await axios.get(
+      `${process.env.REACT_APP_API_URL}/cart/${uid}`
+    );
+
+    const { msg } = result.data;
+    const { cart } = result.data;
+
+    console.log(msg);
+    dispatch({ type: REMOVE_CART_SUCCESS, payload: cart });
+  } catch (err) {
+    const errorCode = err.code;
+    const errorMessage = err.message;
+    console.log(errorCode, errorMessage);
+
+    swal({
+      title: "Request Error",
+      text: `Please try again in a few minutes`,
+      icon: "error",
+    });
+
+    dispatch({ type: REMOVE_CART_ERROR });
+  }
+};
--- src/redux/reducers/cart.reducer.js
+++ src/redux/reducers/cart.reducer.js
@@ -1,7 +1,81 @@
-const initialState = {};
+import {
+  GET_CART_PENDING,
+  GET_CART_SUCCESS,
+  GET_CART_ERROR,
+  INSERT_CART_PENDING,
+  INSERT_CART_SUCCESS,
+  INSERT_CART_ERROR,
+  REMOVE_CART_PENDING,
+  REMOVE_CART_SUCCESS,
+  REMOVE_CART_ERROR,
+} from "../types";
+
+const initialState = {
+  cart: [],
+  isLoading: false,
+};
 
 const cartReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case GET_CART_PENDING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case GET_CART_SUCCESS:
+      return {
+        ...state,
+        cart: action.payload,
+        isLoading: false,
+      };
+
+    case GET_CART_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+      };
+
+    case INSERT_CART_PENDING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case INSERT_CART_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+      };
+
+    case REMOVE_CART_PENDING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case REMOVE_CART_SUCCESS:
+      return {
+        ...state,
+        cart: action.payload,
+        isLoading: false,
+      };
+
+    case REMOVE_CART_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+      };
+
+    case INSERT_CART_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default cartReducer;
