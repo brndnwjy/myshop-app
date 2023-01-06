0ad7c217fed897796d6664a68fa932dbc51d59d9
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-06 15:55:21 +0700

    Add product list page

--- src/pages/product/list/index.js
+++ src/pages/product/list/index.js
@@ -1,10 +1,58 @@
-import React from 'react'
-import styles from "./list.module.css"
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "../../../components/module/navbar";
+
+import styles from "./list.module.css";
+import { useNavigate } from "react-router";
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default List
+  const [products, setProducts] = useState([]);
+
+  const fetchAPI = () => {
+    axios.get(`${process.env.REACT_APP_API_URL}/product`).then((res) => {
+      setProducts(res.data.products);
+    });
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <Fragment>
+      <Navbar />
+      <main className={styles.main}>
+        <h1>Product Page</h1>
+
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Price</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((item, index) => (
+              <tr key={index}>
+                <td>{item.title}</td>
+                <td>{item.stock}</td>
+                <td>{item.price}</td>
+                <td>
+                  <button onClick={() => navigate(`/product/${item.id}`)}>
+                    Buy?
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    </Fragment>
+  );
+};
+
+export default List;
