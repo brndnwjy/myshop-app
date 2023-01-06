6b1b6b12bdc874707cd0f8d6f29c5f3cd3e0e877
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-07 05:27:13 +0700

    Redesign auth page

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   loginWithEmail,
   loginWithGoogle,
 } from "../../../redux/actions/auth.action";
+
+import logo from "../../../assets/myshop-logo.png";
+import google from "../../../assets/google.png";
+import banner from "../../../assets/login-banner.png";
 
 import styles from "../auth.module.css";
 
@@ -28,36 +32,71 @@ const Login = () => {
   };
 
   return (
-    <main>
-      <h1>Login Page</h1>
+    <main className={styles.main}>
+      <img src={logo} alt={"Myshop logo"} />
 
-      <button
-        type="button"
-        onClick={() => handleGoogleLogin()}
-        disabled={isLoading}
-      >
-        {isLoading ? "Logging In" : "Google"}
-      </button>
-      <hr />
-      <input
-        type="email"
-        placeholder="email"
-        onChange={(e) => setForm({ ...form, email: e.target.value })}
-        value={form.email}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        onChange={(e) => setForm({ ...form, password: e.target.value })}
-        value={form.password}
-      />
-      <button
-        type="button"
-        onClick={() => handleEmailLogin()}
-        disabled={isLoading}
-      >
-        {isLoading ? "Logging In" : "Log In"}
-      </button>
+      <div className={styles.content}>
+        <section className={styles.banner}>
+          <img src={banner} alt={"login banner"} />
+          <h2>Belanja Murah Hanya di Myshop</h2>
+          <p>Masuk dan dapatkan potongan harga terbaik di Myshop</p>
+        </section>
+
+        <section className={styles.card}>
+          <div className={styles.header}>
+            <h2>Masuk</h2>
+            <Link to="/register">Daftar</Link>
+          </div>
+
+          <form className={styles.form}>
+            <label htmlFor="email">Email</label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+              value={form.email}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              value={form.password}
+            />
+            <button
+              type="button"
+              onClick={() => handleEmailLogin()}
+              disabled={isLoading}
+              className={
+                isLoading
+                  ? `${styles["login-btn"]} ${styles.muted}`
+                  : styles["login-btn"]
+              }
+            >
+              Log In
+            </button>
+          </form>
+
+          <div className={styles.options}>
+            <h6>
+              <span>atau masuk dengan</span>
+            </h6>
+          </div>
+
+          <button
+            type="button"
+            className={styles["google-btn"]}
+            onClick={() => handleGoogleLogin()}
+            disabled={isLoading}
+          >
+            <img src={google} alt={"Google icon"} width={18} />
+            <span>Google</span>
+          </button>
+        </section>
+      </div>
     </main>
   );
 };
--- src/pages/auth/register/index.js
+++ src/pages/auth/register/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   loginWithGoogle,
   registerWithEmail,
 } from "../../../redux/actions/auth.action";
+import logo from "../../../assets/myshop-logo.png";
+import google from "../../../assets/google.png";
+import banner from "../../../assets/register-banner.png";
 
 import styles from "../auth.module.css";
 
@@ -29,42 +32,79 @@ const Register = () => {
   };
 
   return (
-    <main>
-      <main>
-        <h1>Register Page</h1>
-        <button type="button" onClick={() => handleGoogleLogin()} disabled={isLoading}>
-          {isLoading ? "Logging In" : "Google"}
-        </button>
-        <hr />
-        <input
-          name="username"
-          type="text"
-          placeholder="username"
-          onChange={(e) => setForm({ ...form, username: e.target.value })}
-          value={form.username}
-        />
-        <input
-          name="email"
-          type="email"
-          placeholder="email"
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
-          value={form.email}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="password"
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-          value={form.password}
-        />
-        <button
-          type="button"
-          onClick={() => handleEmailSignup()}
-          disabled={isLoading}
-        >
-          {isLoading ? "Registering" : "Register"}
-        </button>
-      </main>
+    <main className={styles.main}>
+      <img src={logo} alt={"Myshop logo"} />
+
+      <div className={styles.content}>
+        <section className={styles.banner}>
+          <img src={banner} alt={"register banner"} />
+          <h2>Belanja Murah Hanya di Myshop</h2>
+          <p>Gabung dan rasakan kemudahan bertransaksi di Myshop</p>
+        </section>
+
+        <section className={styles.card}>
+          <h2>Daftar Sekarang</h2>
+          <p>
+            Sudah punya akun Myshop? <Link to="/login">Masuk</Link>
+          </p>
+          <form className={styles.form}>
+            <label htmlFor="username">Username</label>
+            <input
+              name="username"
+              type="text"
+              placeholder="username"
+              onChange={(e) => setForm({ ...form, username: e.target.value })}
+              value={form.username}
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+              value={form.email}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              value={form.password}
+            />
+            <button
+              type="button"
+              onClick={() => handleEmailSignup()}
+              disabled={isLoading}
+              className={
+                isLoading
+                  ? `${styles["register-btn"]} ${styles.muted}`
+                  : styles["register-btn"]
+              }
+            >
+              Register
+            </button>
+          </form>
+
+          <div className={styles.options}>
+            <h6>
+              <span>atau masuk dengan</span>
+            </h6>
+          </div>
+
+          <button
+            type="button"
+            onClick={() => handleGoogleLogin()}
+            disabled={isLoading}
+            className={styles["google-btn"]}
+          >
+            <img src={google} alt={"Google icon"} width={18} />
+            <span>Google</span>
+          </button>
+        </section>
+      </div>
     </main>
   );
 };
